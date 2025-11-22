2d7fcedc5448ca11e3e130c6d7377299ed1e8e36
Leonardo Silveira <sombriks@gmail.com>
2025-11-22 15:12:53 -0300

    fancy constructor

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,7 +7,7 @@ import { RouterOutlet } from '@angular/router';
   template: `
     <h1>Welcome to {{ title() }}!</h1>
 
-    <button (click)="counter.set(counter() + 1)">Count is {{counter()}}</button>
+    <button (click)="increment()">Count is {{counter()}}</button>
     <button (click)="doubler = doubler * 2">Double is {{doubler}}</button>
     
     <router-outlet />
@@ -15,7 +15,15 @@ import { RouterOutlet } from '@angular/router';
   styles: [`button { margin: 1rem; }`],
 })
 export class App {
-  protected readonly title = signal('hello-world');
-  protected readonly counter = signal(0);
-  protected doubler = 2;
+
+  constructor(
+    protected readonly title = signal('hello-world'),
+    protected readonly counter = signal(0),
+    protected doubler = 2,) {
+      console.log('init');
+  }
+  
+  protected increment() {
+    this.counter.update(value => value + 1);
+  }
 }
