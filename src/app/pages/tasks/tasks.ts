5925bb695449608c68920e7bbc510120c7ad5e59
Leonardo Silveira <sombriks@gmail.com>
2025-11-23 08:56:39 -0300

    come control flow on template

--- src/app/pages/tasks/tasks.ts
+++ src/app/pages/tasks/tasks.ts
@@ -1,11 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tasks',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tasks.html',
   styleUrl: './tasks.scss',
 })
 export class Tasks {
 
+  readonly tasks : Task[] = [
+    { id: 1, title: 'Task One', completed: false },
+    { id: 2, title: 'Task Two', completed: true },
+    { id: 3, title: 'Task Three', completed: false },
+  ];
+
+  selected : Task = {};
+
+  selectTask(task: any) {
+    this.selected = task;
+  }
 }
+
+export type Task = { 
+  id?: number;
+  title?: string;
+  completed?: boolean;
+ };
